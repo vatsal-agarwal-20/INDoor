7fcdfdd57f0ba8628aac89755f13bf7a402868c6
Ashish Vats <vatsaashish11@gmail.com>
2023-03-05 11:51:41 +0530

    Update main.js

--- Day/assets/js/main.js
+++ Day/assets/js/main.js
@@ -226,7 +226,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 800,
       easing: 'ease-in-out',
       once: true,
       mirror: false
